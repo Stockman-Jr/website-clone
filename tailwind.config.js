9740b8a8893a9020753ed71936b497d78736ab43
Stockman-Jr <stockmanmimmi@gmail.com>
2024-10-22 14:13:53 +0200

    Add news/update section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ["./src/**/*.{html,js}", "./public/**/*.{html,js}"],
   theme: {
+    colors: {
+      "neon-yellow": "rgb(212, 253, 65)",
+    },
     extend: {},
   },
   plugins: [],
